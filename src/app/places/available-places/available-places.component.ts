71c1c2a97937c7192c2aedd94588fcc075284627
jtquangduy <justinquangduy@gmail.com>
2025-02-18 10:33:01 +0700

    Sending Data To A Backend

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -54,4 +54,12 @@ export class AvailablePlacesComponent implements OnInit {
       subcription.unsubscribe();
     });
   }
+
+  onSelectPlace(selectedPlace: Place) {
+    this.httpClient
+      .put('http://localhost:3000/user-places', { placeId: selectedPlace.id })
+      .subscribe({
+        next: (resData) => console.log(resData),
+      });
+  }
 }
